69c1b57304a879c98052a4b4e7d1866d84014c04
Majus Lenz <mariuslenzing@yahoo.de>
2022-01-26 15:09:10 +0100

    initial commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+/**
+ * @author Marius Lenzing
+ * Display font only by drawing shapes surrounding the font without collisions.
+ *
+ * thanks to Ben Moren (p5.collide2d.js)
+ */
+
+/**
+ * P5.js Method
+ * call once before start
+ */
+function preload() {
+}
+
+/**
+ * P5.js Method
+ * call once on start
+ */
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background("black")
+  fill("red")
+  rect(100,100,200,200);
+}
+
+/**
+ * P5.js Method
+ * call once every frame
+ */
+function draw() {
+
+}
