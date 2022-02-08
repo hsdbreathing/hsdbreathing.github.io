1c77ecd28ed2b908c71620f8cc2b7ebeea28bcc0
Majus Lenz <mariuslenzing@yahoo.de>
2022-02-08 20:13:36 +0100

    added click input and stylings

--- sketch.js
+++ sketch.js
@@ -11,11 +11,17 @@ var positionOnSinusCurve = 0;
 
 var breathingSpeed;
 
+var screenOrientation;
+
+var lungIsBeingFilled = false;
+var lungIsBeingEmptied = false;
+
 /**
  * P5.js Method
  * call once before start
  */
 function preload() {
+
 }
 
 /**
@@ -28,6 +34,15 @@ function setup() {
   centerX = windowWidth /2;
   centerY = windowHeight /2;
 
+  if (centerX > centerY) {
+    screenOrientation = "landscape";
+    halfFullDiameter = centerY;
+  }
+  else {
+    screenOrientation = "portrait";
+    halfFullDiameter = centerX;
+  }
+
   noStroke();
 }
 
@@ -36,27 +51,30 @@ function setup() {
  * call once every frame
  */
 function draw() {
+  computeInput();
   drawBackground();
   drawLung();
 }
 
+/** draws background */
 function drawBackground() {
   background("white");
-  fill("black")
+  if (lungIsBeingFilled) {
+    background("#EEEEEEFF");
+  }
+
+  fill("black");
+  if (lungIsBeingEmptied) {
+    fill("#111111FF");
+  }
   rect(0,0, centerX, windowHeight);
 }
 
+/** computes state of breathing animation and renders lung */
 function drawLung() {
   fill("blue");
 
-  if (centerX > centerY) {
-    halfFullDiameter = centerY;
-  }
-  else {
-    halfFullDiameter = centerX;
-  }
-
-  maxBreathingDiameter = halfFullDiameter /4;
+  maxBreathingDiameter = halfFullDiameter /6;
 
   breathingSpeed = 1;
 
@@ -65,4 +83,34 @@ function drawLung() {
   circle(centerX, centerY, currentDiameter);
 
   positionOnSinusCurve += .02 * breathingSpeed;
+}
+
+/** takes mouse input to increase or decrease lung */
+function computeInput() {
+  lungIsBeingFilled = false;
+  lungIsBeingEmptied = false;
+
+  if (mouseIsPressed) {
+
+    if (mouseX > centerX) {
+      lungIsBeingFilled = true;
+      fillLung();
+    }
+    else {
+      lungIsBeingEmptied = true;
+      emptyLung();
+    }
+  }
+}
+
+/** makes lung bigger by tiny amount */
+function fillLung() {
+  halfFullDiameter += 0.1;
+  //console.log("fill");
+}
+
+/** makes lung smaller by tiny amount */
+function emptyLung() {
+  halfFullDiameter -= 0.1;
+  //console.log("empty");
 }
